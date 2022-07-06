e9239402e4b12be65818815bce1cdacec4939595
Andrey Reznikov <reznikovandrusha@gmail.com>
2022-07-06 22:25:22 +0500

    add time calculations
    - Install `dayjs` lib
    - Remove unused code, add function for manipulation with dates in `utils.js`
    - Build `data` object for table

--- src/components/UsersStatistics.jsx
+++ src/components/UsersStatistics.jsx
@@ -1,13 +1,24 @@
 import PropTypes from 'prop-types';
 import { Table } from 'antd';
 
+import { calcSummaryStatForDay } from '../utils';
+
 const { Column } = Table;
 
 const UsersStatistics = ({ usersStats }) => {
-  const data = usersStats.map(({ id, Fullname: fullname }) => ({
-    key: id,
-    User: fullname,
-  }));
+  const data = usersStats.map(({ id, Fullname: fullname, Days: days }) => {
+    const usersMonthStat = days.reduce((acc, { Date: date, End: end, Start: start }) => {
+      const day = new Date(date).getDate();
+      const dayStat = calcSummaryStatForDay(start, end);
+      return { ...acc, [day]: dayStat };
+    }, {});
+
+    return {
+      key: id,
+      User: fullname,
+      ...usersMonthStat,
+    };
+  });
 
   return (
     <Table dataSource={data}>
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,16 @@
-const DAYS_QUANTITY = 31;
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+
+dayjs.extend(customParseFormat);
 
 export const apiUrl = '/api/users';
 
-export const getMonthDaysArr = () => [...Array(DAYS_QUANTITY + 1).keys()];
+export const calcSummaryStatForDay = (start, end) => {
+  const timeTo = dayjs(start, 'HH-mm');
+  const timeFrom = dayjs(end, 'HH-mm');
+
+  return timeTo
+    .subtract(timeFrom.hour(), 'hours')
+    .subtract(timeFrom.minute(), 'minutes')
+    .format('HH-mm');
+};
