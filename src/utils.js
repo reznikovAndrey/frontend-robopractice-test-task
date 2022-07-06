bbda67d9d370c7986b5cb3fc1606666efb8f93c3
Andrey Reznikov <reznikovandrusha@gmail.com>
2022-07-07 02:08:33 +0500

    fix: bug with calc stats function
    - Swap props for `timeTo` and `timeFrom`
    - Fix output format

--- src/utils.js
+++ src/utils.js
@@ -6,11 +6,11 @@ dayjs.extend(customParseFormat);
 export const apiUrl = '/api/users';
 
 export const calcSummaryStatForDay = (start, end) => {
-  const timeTo = dayjs(start, 'HH-mm');
-  const timeFrom = dayjs(end, 'HH-mm');
+  const timeTo = dayjs(end, 'HH-mm');
+  const timeFrom = dayjs(start, 'HH-mm');
 
   return timeTo
     .subtract(timeFrom.hour(), 'hours')
     .subtract(timeFrom.minute(), 'minutes')
-    .format('HH-mm');
+    .format('HH:mm');
 };
